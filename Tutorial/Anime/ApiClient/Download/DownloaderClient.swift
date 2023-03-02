437c661f2b420eaa2f050039c58bdf70af4c5a94
Ongar.dev <91256870+developer-ongar@users.noreply.github.com>
2023-03-02 15:35:54 +0600

    Create DownloaderClient.swift

---
+++ Tutorial/Anime/ApiClient/Download/DownloaderClient.swift
@@ -0,0 +1,129 @@
+//
+//  DownloaderClient.swift
+//
+
+import Combine
+import ComposableArchitecture
+import Foundation
+import SharedModels
+
+// MARK: - DownloaderClient
+
+public struct DownloaderClient {
+    public let observe: (Anime.ID?) -> AsyncStream<Set<AnimeStorage>>
+    public let download: (Request) -> Void
+    public let delete: (Anime.ID, Int) async -> Void
+    public let count: () -> AsyncStream<Int>
+    public let cancel: (Anime.ID, Int) async -> Void
+    public let retry: (Anime.ID, Int) async -> Void
+    public let reset: () async -> Void
+}
+
+public extension DownloaderClient {
+    enum Status: Hashable, Codable {
+        case pending
+        case downloading(progress: Double)
+        case downloaded(location: URL)
+        case failed
+
+        public var canCancelDownload: Bool {
+            switch self {
+            case .pending, .downloading:
+                return true
+            default:
+                return false
+            }
+        }
+    }
+
+    struct Request {
+        let anime: any AnimeRepresentable
+        let episode: any EpisodeRepresentable
+        let source: Source
+
+        public init(
+            anime: any AnimeRepresentable,
+            episode: any EpisodeRepresentable,
+            source: Source
+        ) {
+            self.anime = anime
+            self.episode = episode
+            self.source = source
+        }
+    }
+
+    struct AnimeStorage: AnimeRepresentable, Codable {
+        public let id: Int
+        public var malId: Int? { nil }
+        public let title: String
+        public let format: Anime.Format
+        public let posterImage: [ImageSize]
+        public var episodes: Set<EpisodeStorage>
+    }
+
+    struct EpisodeStorage: EpisodeRepresentable, Codable {
+        public var id: Int { number }
+        public let number: Int
+        public let title: String
+        public let thumbnail: ImageSize?
+        public var isFiller: Bool
+
+        public var status: Status
+
+        public var links: Set<EpisodeLink> {
+            switch status {
+            case let .downloaded(location: url):
+                return [.offline(url: url)]
+            default:
+                return []
+            }
+        }
+    }
+}
+
+public extension DownloaderClient.EpisodeStorage {
+    var isDownloading: Bool {
+        switch status {
+        case .downloading:
+            return true
+        default:
+            return false
+        }
+    }
+}
+
+public extension DownloaderClient.AnimeStorage {
+    var downloadingCount: Int {
+        episodes.filter(\.isDownloading).count
+    }
+
+    var downloadingProgress: Double {
+        episodes.filter(\.isDownloading).reduce(0.0) { partialResult, episode in
+            if case let .downloading(progress) = episode.status {
+                return partialResult + progress
+            } else {
+                return partialResult
+            }
+        } / max(Double(downloadingCount), 1.0)
+    }
+}
+
+public extension DownloaderClient.EpisodeStorage {
+    var fileExists: Bool {
+        if case let .downloaded(location) = status {
+            return FileManager.default.fileExists(atPath: location.path)
+        }
+        return false
+    }
+}
+
+// MARK: - DownloaderClient + DependencyKey
+
+extension DownloaderClient: DependencyKey {}
+
+public extension DependencyValues {
+    var downloaderClient: DownloaderClient {
+        get { self[DownloaderClient.self] }
+        set { self[DownloaderClient.self] = newValue }
+    }
+}
