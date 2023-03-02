51abd3d8446e638fb4e9feebb2b6e287b1cb22b6
Ongar.dev <91256870+developer-ongar@users.noreply.github.com>
2023-03-02 15:27:35 +0600

    Create  AnimeClient+Mock.swift

---
+++ Tutorial/Anime/ApiClient/AnimeClient+Mock.swift
@@ -0,0 +1,36 @@
+//
+//  AnimeClient+Mock.swift
+//
+
+import ComposableArchitecture
+import SharedModels
+
+public extension AnimeClient {
+    static let previewValue = Self {
+        []
+    } getTopUpcomingAnime: {
+        []
+    } getTopAiringAnime: {
+        []
+    } getHighestRatedAnime: {
+        []
+    } getMostPopularAnime: {
+        []
+    } getAnimes: { _ in
+        []
+    } getAnime: { _ in
+        throw AnimeClient.Error.providerNotAvailable
+    } searchAnimes: { _ in
+        []
+    } getRecentlyUpdated: {
+        []
+    } getEpisodes: { _, _ in
+        .init(name: "", episodes: [Episode(title: "", number: 0, description: "", isFiller: true)])
+    } getSources: { _, _ in
+        .init([])
+    } getSkipTimes: { _, _ in
+        .init()
+    } getAnimeProviders: {
+        []
+    }
+}
