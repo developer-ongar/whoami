9f16fa32ee1b6f439023d8159ad87964e5540acf
Ongar.dev <91256870+developer-ongar@users.noreply.github.com>
2023-03-02 15:33:28 +0600

    Create DiscordClient+Live.swift

---
+++ Tutorial/Anime/ApiClient/Discord/DiscordClient+Live.swift
@@ -0,0 +1,81 @@
+//
+//  File.swift
+//
+
+#if os(macOS)
+
+import APIClient
+import ComposableArchitecture
+import Foundation
+import SwordRPC
+
+public extension DiscordClient {
+    static let liveValue: Self = {
+        let sword = SwordRPC(
+            appId: AnimeNowAPI.discordClientKey,
+            maxRetryCount: 0
+        )
+
+        return .init(isActive: sword.isRunning, isConnected: sword.isConnected) {
+            .init { continuation in
+                let task = Task.detached {
+                    while true {
+                        if sword.isRunning {
+                            if sword.isConnected {
+                                continuation.yield(.connected)
+                            } else {
+                                continuation.yield(.failed)
+                            }
+                        } else {
+                            continuation.yield(.offline)
+                        }
+                        try? await Task.sleep(nanoseconds: 1_000_000_000 * 5)
+                    }
+                }
+
+                continuation.onTermination = { _ in
+                    task.cancel()
+                }
+            }
+        } setActive: { active in
+            if active, !sword.isRunning {
+                sword.start()
+            } else if !active, sword.isRunning {
+                sword.stop()
+            }
+        } setActivity: { activity in
+            switch activity {
+            case .none:
+                sword.setPresence(nil)
+
+            case let .some(.watching(info)):
+                var presence = RichPresence()
+                presence.state = info.name
+                presence.details = info.episode
+                presence.assets.largeText = info.episode
+                presence.assets.largeImage = info.image
+                presence.assets.smallImage = "logo"
+                presence.assets.smallText = "Anime Now!"
+
+                if info.duration > 0 {
+                    let main = Date()
+                    let currentProgress = info.progress * info.duration
+                    presence.timestamps.start = main - currentProgress
+                    presence.timestamps.end = main + info.duration - currentProgress
+                }
+
+                var button = RichPresence.Button()
+                button.label = "Watch Now"
+                button.url = "https://animenow.app"
+                presence.buttons.append(button)
+
+                sword.setPresence(presence)
+
+            case .some:
+                break
+            }
+        }
+    }()
+}
+
+#endif
